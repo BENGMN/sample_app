8d9c6ee7df4673a4ba88483ce1214f8f03954a72
Ben Crudo <ben.crudo@gmail.com>
2010-11-08 22:44:02 -0500

    User signup complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,24 @@
 class UsersController < ApplicationController
   def new 
-	@title = "Sign up"
+    @title = "Sign up"
+    @user = User.new  # Create a new user object in the contructor
   end
 
+  # This is the controller method which will locate and display a particular user
   def show
-	@user = User.find(params[:id])
-	@title = User.name
+    @user = User.find(params[:id])
+    @title = User.name
   end
 
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      @title = "Sign up"
+      @user.password = ""
+      render 'new'
+    end
+  end
 end
